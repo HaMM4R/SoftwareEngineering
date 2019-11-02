565f57d3df865754a1a2b033eee1fd1554d1dbab
HaMM4R <HaMM4RTime@hotmail.com>
2019-11-02 15:29:55 +0000

    Initial Commit
    Initial Commit of spotify project.

---
+++ SpotifyApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+
+namespace SpotifyApp
+{
+    class Program
+    {
+        private static string _clientId = "a1e7f499b35149eb9b0300caf0e428bc"; //"";
+        private static string _secretId = "0ebd63a52739483c98a2731aeaf00516"; //"";
+        private static SpotifyWebAPI _spotify;
+
+        static bool setup = false;
+
+        internal static void Main(string[] args)
+        {
+            SearchSpotifyAlbums spot;
+            _clientId = string.IsNullOrEmpty(_clientId)
+                ? Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID")
+                : _clientId;
+
+            _secretId = string.IsNullOrEmpty(_secretId)
+                ? Environment.GetEnvironmentVariable("SPOTIFY_SECRET_ID")
+                : _secretId;
+
+
+            AuthorizationCodeAuth auth =
+                new AuthorizationCodeAuth(_clientId, _secretId, "http://localhost:4002", "http://localhost:4002",
+                    Scope.PlaylistReadPrivate | Scope.PlaylistReadCollaborative);
+            auth.AuthReceived += AuthOnAuthReceived;
+            auth.Start();
+            auth.OpenBrowser();
+
+            while(!setup)
+            {
+                if (setup)
+                {
+                    spot = new SearchSpotifyAlbums(_spotify);
+                }
+            }
+            
+            Console.ReadLine();
+            auth.Stop(0);
+        }
+
+
+        private static async void AuthOnAuthReceived(object sender, AuthorizationCode payload)
+        {
+            AuthorizationCodeAuth auth = (AuthorizationCodeAuth)sender;
+            auth.Stop();
+
+            Token token = await auth.ExchangeCode(payload.Code);
+            SpotifyWebAPI api = new SpotifyWebAPI
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType
+            };
+
+            _spotify = api;
+            setup = true;
+        }
+
+
+        private static async void PrintUsefulData(SpotifyWebAPI api, string test)
+        {
+            PrivateProfile profile = await api.GetPrivateProfileAsync();
+            string name = string.IsNullOrEmpty(profile.DisplayName) ? profile.Id : profile.DisplayName;
+            Console.WriteLine($"Hello there, {name}!");
+
+            SearchItem search = api.SearchItemsEscaped(test, SearchType.Album);
+            Paging<SimpleAlbum> albums = search.Albums;
+            albums.Items.ForEach(item => Console.WriteLine(item.Name));
+
+            Console.WriteLine("Your playlists:");
+            Paging<SimplePlaylist> playlists = api.GetUserPlaylists(profile.Id);
+            playlists.Items.ForEach(playlist =>
+            {
+                Console.WriteLine($" - {playlist.Tracks.Total} ");
+                Console.WriteLine($"- {playlist.Name}");
+            });
+        }
+
+    }
+}
