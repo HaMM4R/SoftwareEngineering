7705131d1f2be3f1683be6e0ddf45dc7ad5d28a4
Joshkad-git <56259669+Joshkad-git@users.noreply.github.com>
2019-11-07 14:29:00 +0000

    Setting up windows form
    Adding buttons and lists to the windows form.

--- SpotifyUI/Form1.Designer.cs
+++ SpotifyUI/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.searchSpotify_btn = new System.Windows.Forms.Button();
+            this.headlines_list = new System.Windows.Forms.ListBox();
+            this.songs_list = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // searchSpotify_btn
+            // 
+            this.searchSpotify_btn.Location = new System.Drawing.Point(12, 373);
+            this.searchSpotify_btn.Name = "searchSpotify_btn";
+            this.searchSpotify_btn.Size = new System.Drawing.Size(75, 23);
+            this.searchSpotify_btn.TabIndex = 0;
+            this.searchSpotify_btn.Text = "button1";
+            this.searchSpotify_btn.UseVisualStyleBackColor = true;
+            this.searchSpotify_btn.Click += new System.EventHandler(this.searchSpotify_btn_Click);
+            // 
+            // headlines_list
+            // 
+            this.headlines_list.FormattingEnabled = true;
+            this.headlines_list.Location = new System.Drawing.Point(12, 127);
+            this.headlines_list.Name = "headlines_list";
+            this.headlines_list.Size = new System.Drawing.Size(120, 95);
+            this.headlines_list.TabIndex = 1;
+            this.headlines_list.SelectedIndexChanged += new System.EventHandler(this.headlines_list_SelectedIndexChanged);
+            // 
+            // songs_list
+            // 
+            this.songs_list.FormattingEnabled = true;
+            this.songs_list.Location = new System.Drawing.Point(239, 127);
+            this.songs_list.Name = "songs_list";
+            this.songs_list.Size = new System.Drawing.Size(112, 95);
+            this.songs_list.TabIndex = 2;
+            this.songs_list.SelectedIndexChanged += new System.EventHandler(this.songs_list_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.songs_list);
+            this.Controls.Add(this.headlines_list);
+            this.Controls.Add(this.searchSpotify_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button searchSpotify_btn;
+        private System.Windows.Forms.ListBox headlines_list;
+        private System.Windows.Forms.ListBox songs_list;
     }
 }
 
--- SpotifyUI/Form1.cs
+++ SpotifyUI/Form1.cs
@@ -16,5 +16,20 @@ namespace SpotifyUI
         {
             InitializeComponent();
         }
+
+        private void searchSpotify_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void headlines_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void songs_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
